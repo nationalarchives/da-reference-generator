0049630787a4517c6ea59af9a9c1952ac1e8b1c2
TomJKing <tom.king@nationalarchives.gov.uk>
2023-08-15 08:51:05 +0100

    Rename table

--- terraform/root_main.tf
+++ terraform/root_main.tf
@@ -5,7 +5,7 @@ module "terraform_config_hosting_project" {
 
 module "dynamodb" {
   source                         = "./da-terraform-modules/dynamo"
-  table_name                     = "${var.project}-reference-counter-test"
+  table_name                     = "${var.project}-reference-counter"
   hash_key                       = local.dynamodb_hash_key
   hash_key_type                  = "S"
   deletion_protection_enabled    = true
