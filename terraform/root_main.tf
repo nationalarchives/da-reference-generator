aee911d45a0579a98d9a79bc94d26d898156306b
thanhz <tri.lam21@outlook.com>
2023-08-23 15:43:27 +0100

    provide correct policies value

--- terraform/root_main.tf
+++ terraform/root_main.tf
@@ -27,11 +27,13 @@ module "reference_generator_lambda" {
   source        = "./da-terraform-modules/lambda"
   function_name = local.reference_generator_function_name
   handler       = "uk.gov.nationalarchives.referencegenerator.Lambda::handleRequest"
-  policies = templatefile("${path.module}/templates/lambda/reference_generator_policy.json.tpl", {
-    function_name = local.reference_generator_function_name
-    account_id    = data.aws_caller_identity.current.account_id
-    table_name    = local.reference_counter_table_name
-  })
+  policies = {
+    "ReferenceGeneratorLambdaPolicy${title(local.hosting_environment)}" = templatefile("${path.module}/templates/lambda/reference_generator_policy.json.tpl", {
+      function_name = local.reference_generator_function_name
+      account_id    = data.aws_caller_identity.current.account_id
+      table_name    = local.reference_counter_table_name
+    })
+  }
   runtime = "java11"
   tags    = local.hosting_common_tags
 }
