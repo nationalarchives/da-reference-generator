8fc4852ae1ece626318f05b62fad60ad8d0d6368
Thanh <tri.lam21@outlook.com>
2024-01-08 12:22:50 +0000

    Update oci-tools dependency Prefix references with 'Z'

--- src/main/scala/uk/gov/nationalarchives/referencegenerator/Encoder.scala
+++ src/main/scala/uk/gov/nationalarchives/referencegenerator/Encoder.scala
@@ -4,14 +4,15 @@ import uk.gov.nationalarchives.oci.Alphabet.Alphabet
 import uk.gov.nationalarchives.oci.{Alphabet, BaseCoder, IncludedAlphabet}
 
 object Encoder {
-  val alphabet: Alphabet = Alphabet.loadAlphabet(Right(IncludedAlphabet.GCRb25)) match {
-    case Left(error) => throw new Exception(s"Error loading the GCRb25 encoding alphabet", error)
+  val alphabet: Alphabet = Alphabet.loadAlphabet(Right(IncludedAlphabet.CTDb25)) match {
+    case Left(error) => throw new Exception(s"Error loading the CTDb25 encoding alphabet", error)
     case Right(alphabet) => alphabet
   }
   val baseNumber = 25
 
   def encode(counter: Long): String = {
     val alphabetIndices = BaseCoder.encode(counter, baseNumber)
-    Alphabet.toString(alphabet, alphabetIndices)
+    val prefix = 'Z'
+    prefix +: Alphabet.toString(alphabet, alphabetIndices)
   }
 }
