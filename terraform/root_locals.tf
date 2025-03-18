1c28fd5317dbc893ddc41bb3e69b90953501a67c
TomJKing <tom.king@nationalarchives.gov.uk>
2025-03-18 11:03:09 +0000

    Update aws backup tag

--- terraform/root_locals.tf
+++ terraform/root_locals.tf
@@ -12,7 +12,7 @@ locals {
   })
 
 
-  aws_backup_tag              = local.hosting_environment == "prod" ? module.terraform_config_hosting_project.terraform_config["aws_backup_7_day_no_cold_tag"] : null
+  aws_backup_tag              = local.hosting_environment == "prod" ? module.terraform_config_hosting_project.terraform_config["aws_backup_daily_short_term_retain_tag"] : null
   aws_backup_service_role_arn = module.aws_backup_terraform_config.terraform_config["aws_service_backup_role"]
   aws_backup_local_role_name  = module.aws_backup_terraform_config.terraform_config["local_account_backup_role_name"]
   aws_backup_local_role_arn   = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.aws_backup_local_role_name}"
