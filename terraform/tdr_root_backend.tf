f621253381fadb8a13e3fa1497bad0fe803111d7
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2025-07-02 19:22:48 +0100

    fix: tf fmt

--- terraform/tdr_root_backend.tf
+++ terraform/tdr_root_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tdr-terraform-state"
-    key            = "reference-generator.state"
-    region         = "eu-west-2"
-    encrypt        = true
-    use_lockfile   = true
+    bucket       = "tdr-terraform-state"
+    key          = "reference-generator.state"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
